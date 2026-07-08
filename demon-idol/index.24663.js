9a4f96c334a0cb0578677c77b2d2f03cf56a93b1
rxxdev <rx_byte.dev@proton.me>
2026-07-08 16:45:55 +0800

    [v1.0.2] Re-Deploy Demon Idol

--- demon-idol/index.24663.js
+++ demon-idol/index.24663.js
@@ -1,4 +1,4 @@
-System.register(["./application.8310d.js"], function (_export, _context) {
+System.register(["./application.d307d.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
