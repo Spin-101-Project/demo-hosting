d4a20dfa2f6dc5388d33da7ef2a3217613643f3f
rxxdev <rx_byte.dev@proton.me>
2026-07-08 11:11:45 +0800

    [v1.0.2] Deploy Demon Idol

--- demon-idol/application.8310d.js
+++ demon-idol/application.8310d.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.e3b65.json';
+          this.settingsPath = 'src/settings.0d91c.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- demon-idol/index.963b1.js
+++ demon-idol/index.963b1.js
@@ -1,4 +1,4 @@
-System.register(["./application.67361.js"], function (_export, _context) {
+System.register(["./application.8310d.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
