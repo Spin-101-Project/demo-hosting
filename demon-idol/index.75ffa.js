56d6ea2f2cf9fcf409091dde6c4727c449ff850e
rxxdev <rx_byte.dev@proton.me>
2026-07-08 17:26:20 +0800

    [v1.0.3] Deploy Demon Idol

--- demon-idol/index.75ffa.js
+++ demon-idol/index.75ffa.js
@@ -1,4 +1,4 @@
-System.register(["./application.d307d.js"], function (_export, _context) {
+System.register(["./application.d4301.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
